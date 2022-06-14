5cb98dce2f80e40f6b281144166b99b284273f87
LambdaXymox <lambda.xymox@gmail.com>
2022-06-14 13:39:59 -0500

    Define lighting scene space objects.

--- src/light/light.rs
+++ src/light/light.rs
@@ -13,10 +13,22 @@ pub struct LightingQuery {
     pub point: Vector3<f32>,
 }
 
+impl LightingQuery {
+    pub fn new(ray_incoming: Vector3<f32>, point: Vector3<f32>) -> Self {
+        Self { ray_incoming, point, }
+    }
+}
+
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct LightingResult {
     pub ray_incoming: Vector3<f32>,
     pub point: Vector3<f32>,
     pub emission: Vector3<f32>,
+}
+
+impl LightingResult {
+    pub fn new(ray_incoming: Vector3<f32>, point: Vector3<f32>, emission: Vector3<f32>) -> Self {
+        Self { ray_incoming, point, emission, }
+    }
 }
 
--- src/scene/scene_object.rs
+++ src/scene/scene_object.rs
@@ -1,3 +1,4 @@
+use crate::light::*;
 use crate::sampler::*;
 use crate::query::*;
 use crate::scene::*;
@@ -98,6 +99,121 @@ impl SceneScatteringObject {
         let result_model_space = self.object.scatter(&query_model_space, sampler);
         
         self.scattering_result_model_space_to_world_space(&result_model_space)
+    }
+
+    #[inline]
+    pub fn center(&self) -> Vector3<f32> {
+        (self.model_matrix * self.object.center().extend(1_f32)).contract()
+    }
+}
+
+
+#[derive(Debug)]
+pub struct SceneLightingObject {
+    object: Box<dyn ModelSpaceLightingObject>,
+    pub model_matrix: Matrix4x4<f32>,
+    model_matrix_inv: Matrix4x4<f32>,
+}
+
+impl SceneLightingObject {
+    pub fn new(object: Box<dyn ModelSpaceLightingObject>, model_matrix: Matrix4x4<f32>) -> Self {
+        let model_matrix_inv = model_matrix.inverse().unwrap();
+        
+        Self { object, model_matrix, model_matrix_inv, }
+    }
+
+    #[inline]
+    fn ray_world_space_to_model_space(&self, ray: &Ray) -> Ray {
+        let ray_origin_model_space = (self.model_matrix_inv * ray.origin.extend(1_f32)).contract();
+        let ray_direction_model_space = (self.model_matrix_inv * ray.direction.extend(0_f32)).contract();
+        
+        Ray::new(ray_origin_model_space, ray_direction_model_space)
+    }
+
+    #[inline]
+    fn intersection_query_world_space_to_model_space(&self, query: &IntersectionQuery) -> IntersectionQuery {
+        let ray_model_space = self.ray_world_space_to_model_space(&query.ray);
+
+        IntersectionQuery::new(ray_model_space, query.t_min, query.t_max)
+    }
+
+    #[inline]
+    fn intersection_result_model_space_to_world_space(&self, result: &IntersectionResult) -> IntersectionResult {
+        if let IntersectionResult::Hit(result_model_space) = result {
+            let result_t_world_space = result_model_space.t;
+            let result_p_world_space = (self.model_matrix * result_model_space.point.extend(1_f32)).contract();
+            let result_normal_world_space = (self.model_matrix * result_model_space.normal.extend(0_f32)).contract();
+
+            IntersectionResult::new_hit(
+                result_t_world_space,
+                result_p_world_space,
+                result_normal_world_space,
+            )
+        } else if let IntersectionResult::Tangent(result_model_space) = result {
+            let result_t_world_space = result_model_space.t;
+            let result_p_world_space = (self.model_matrix * result_model_space.point.extend(1_f32)).contract();
+            let result_normal_world_space = (self.model_matrix * result_model_space.normal.extend(0_f32)).contract();
+
+            IntersectionResult::new_tangent(
+                result_t_world_space,
+                result_p_world_space,
+                result_normal_world_space,
+            )
+        } else {
+            // The ray missed the object.
+            *result
+        }
+    }
+
+    pub fn intersect(&self, query: &IntersectionQuery) -> IntersectionResult {
+        let query_model_space = self.intersection_query_world_space_to_model_space(query);
+        let result_model_space = self.object.intersect(&query_model_space);
+
+        self.intersection_result_model_space_to_world_space(&result_model_space)
+    }
+
+    fn scattering_query_world_space_to_model_space(&self, query: &ScatteringQuery) -> ScatteringQuery {
+        let ray_incoming_model_space = (self.model_matrix_inv * query.ray_incoming.extend(0_f32)).contract();
+        let point_model_space = (self.model_matrix_inv * query.point.extend(1_f32)).contract();
+
+        ScatteringQuery::new(ray_incoming_model_space, point_model_space)
+    }
+
+    fn scattering_result_model_space_to_world_space(&self, result: &ScatteringResult) -> ScatteringResult {
+        let ray_incoming_model_space = (self.model_matrix * result.ray_incoming.extend(0_f32)).contract();
+        let ray_outgoing_model_space = (self.model_matrix * result.ray_outgoing.extend(0_f32)).contract();
+        let point_model_space = (self.model_matrix * result.point.extend(1_f32)).contract();
+        let normal_model_space = (self.model_matrix * result.normal.extend(0_f32)).contract();
+
+        ScatteringResult::new(
+            ray_incoming_model_space,
+            ray_outgoing_model_space,
+            point_model_space,
+            normal_model_space,
+            result.scattering_fraction
+        )
+    }
+
+    fn lighting_query_world_space_to_model_space(&self, query: &LightingQuery) -> LightingQuery {
+        let ray_incoming_model_space = (self.model_matrix_inv * query.ray_incoming.extend(0_f32)).contract();
+        let point_model_space = (self.model_matrix_inv * query.point.extend(1_f32)).contract();
+
+        LightingQuery::new(ray_incoming_model_space, point_model_space)
+    }
+
+    fn lighting_result_model_space_to_world_space(&self, result: &LightingResult) -> LightingResult {
+        let ray_incoming_world_space = (self.model_matrix * result.ray_incoming.extend(0_f32)).contract(); 
+        let point_world_space = (self.model_matrix * result.point.extend(1_f32)).contract();
+        let emission = result.emission;
+
+        LightingResult::new(ray_incoming_world_space, point_world_space, emission)
+    }
+
+    pub fn emit(&self, query: &LightingQuery) -> LightingResult {
+        let query_model_space = self.lighting_query_world_space_to_model_space(query);
+        let result_model_space = self.object.emit(&query_model_space);
+        
+        self.lighting_result_model_space_to_world_space(&result_model_space)
     }
 
     #[inline]
